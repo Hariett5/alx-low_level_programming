9c2896e160d669fc43871a9f64b9d00d6f8b7919
Hariett5 <nyaale3@gmail.com>
2023-06-23 13:19:53 -0700

    Dynamic libraries

--- 0x18-dynamic_libraries/1-strncat.c
+++ 0x18-dynamic_libraries/1-strncat.c
@@ -1,18 +1,17 @@
 #include "main.h"
 /**
- * _strncat - Concatenates two strings,
- * @dest: the destination.
- * @src: the source.
- * @n: the amount of bytes used from src.
- * Return: the pointer to dest.
- */
+* _strncat - Concatenates two strings,
+* @dest: the destination.
+* @src: the source.
+* @n: the amount of bytes used from src.
+* Return: the pointer to dest.
+*/
 char *_strncat(char *dest, char *src, int n)
 {
-	int i;
-	int j;
-
-	i = 0;
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
+int i;
+int j;
+i = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
